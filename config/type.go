99ec5a673818a8a89902a777d75e353d004c26cf
Hossein Araghi <hoseinaraghi84@gmail.com>
2024-12-21 14:29:06 +0330

    feat(config): Implement config type and config reader

---
+++ config/type.go
@@ -0,0 +1,29 @@
+package config
+
+type Config struct {
+	DB     DBConfig     `json:"db"`
+	Server ServerConfig `json:"server"`
+	Redis  RedisConfig  `json:"redis"`
+}
+
+type DBConfig struct {
+	Host   string `json:"host"`
+	Port   uint   `json:"port"`
+	User   string `json:"user"`
+	Pass   string `json:"pass"`
+	Name   string `json:"name"`
+	Schema string `json:"schema"`
+}
+
+type ServerConfig struct {
+	Token           string `json:"token"`
+	Proxy           string `json:"proxy"`
+	Port            uint   `json:"port"`
+	NewUpdateOffset uint   `json:"new_update_offset"`
+	BotTimeout      uint   `json:"bot_timeout"`
+}
+
+type RedisConfig struct {
+	Host string `json:"host"`
+	Port uint   `json:"port"`
+}
