0b8f47318a9bb91fe91c520e28c02f98b506a011
Hossein Araghi <hoseinaraghi84@gmail.com>
2025-01-05 21:18:47 +0330

    feat: Better command menu handling

--- pkg/bot/utils.go
+++ pkg/bot/utils.go
@@ -3,13 +3,14 @@ package bot
 import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"log"
+	"slices"
 	"taha_tahvieh_tg_bot/app"
 )
 
-func sendMessage(ac app.App, update tgbotapi.Update, text string, html bool) {
-	msg := tgbotapi.NewMessage(update.Message.Chat.ID, text)
+func sendMessage(ac app.App, update tgbotapi.Update, text string, markdown bool) {
+	msg := tgbotapi.NewMessage(update.FromChat().ID, text)
 
-	if html {
+	if markdown {
 		msg.ParseMode = tgbotapi.ModeMarkdown
 	}
 
@@ -21,10 +22,46 @@ func sendMessage(ac app.App, update tgbotapi.Update, text string, html bool) {
 	}
 }
 
+func SendMessage(ac app.App, c tgbotapi.Chattable) {
+	_, err := ac.Bot().Send(c)
+
+	if err != nil {
+		log.Println("Error sending message:", err)
+		return
+	}
+}
+
+func SendRequest(ac app.App, c tgbotapi.Chattable) {
+	_, err := ac.Bot().Request(c)
+
+	if err != nil {
+		log.Println("Error sending message:", err)
+		return
+	}
+}
+
 func SendText(ac app.App, update tgbotapi.Update, text string) {
 	sendMessage(ac, update, text, false)
 }
 
-func SendHtml(ac app.App, update tgbotapi.Update, text string) {
+func SendMarkdown(ac app.App, update tgbotapi.Update, text string) {
 	sendMessage(ac, update, text, true)
+}
+
+func IsAdmin(update tgbotapi.Update, ac app.App) bool {
+	constants := ac.Config().Constants
+	username := update.SentFrom().UserName
+
+	return slices.Contains(constants.Admins, username)
+}
+
+func IsSuperAdmin(update tgbotapi.Update, ac app.App) bool {
+	constants := ac.Config().Constants
+	username := update.SentFrom().UserName
+
+	return slices.Contains(constants.SuperAdmins, username)
+}
+
+func IsSuperRole(update tgbotapi.Update, ac app.App) bool {
+	return IsAdmin(update, ac) || IsSuperAdmin(update, ac)
 }
