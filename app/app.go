bb3f8a8f06f51f4d79399479ec9f667edf35ad13
Hossein Araghi <hoseinaraghi84@gmail.com>
2025-01-05 18:27:04 +0330

    feat: Add missing methods to DI interface, fix bugs and implement missing logics

--- app/app.go
+++ app/app.go
@@ -1,13 +1,23 @@
 package app
 
 import (
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"context"
+	"gorm.io/gorm"
 	"taha_tahvieh_tg_bot/config"
+	"taha_tahvieh_tg_bot/internal/settings"
+	"taha_tahvieh_tg_bot/pkg/adapters/storage"
+	"taha_tahvieh_tg_bot/pkg/postgres"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	settingsPort "taha_tahvieh_tg_bot/internal/settings/port"
 )
 
 type app struct {
-	cfg config.Config
-	bot *tgbotapi.BotAPI
+	cfg             config.Config
+	ctx             context.Context
+	db              *gorm.DB
+	bot             *tgbotapi.BotAPI
+	settingsService settingsPort.Service
 }
 
 func (a *app) Config() config.Config {
@@ -18,27 +28,58 @@ func (a *app) Bot() *tgbotapi.BotAPI {
 	return a.bot
 }
 
-func (a *app) setBot(bot *tgbotapi.BotAPI) {
-	a.bot = bot
+func (a *app) DB() *gorm.DB {
+	return a.db
 }
 
-func NewApp(cfg config.Config, bot *tgbotapi.BotAPI) (App, error) {
-	a := &app{cfg: cfg}
+func (a *app) SettingsService() settingsPort.Service {
+	if a.settingsService == nil {
+		if a.db != nil {
+			a.settingsService = settings.NewService(a.ctx, storage.NewSettingRepo(a.db))
 
-	a.setBot(bot)
+			if err := a.settingsService.RunMigrations(); err != nil {
+				panic("failed to run migrations")
+			}
 
-	/*	if err := a.setDB(); err != nil {
-			return nil, err
+			return a.settingsService
 		}
 
-		a.setRedis()
-		a.setEmailService()*/
+		return nil
+	}
+
+	return a.settingsService
+}
+
+func (a *app) setBot() error {
+	db, err := postgres.NewPsqlGormConnection(postgres.DBConnOptions{
+		Host:   a.cfg.DB.Host,
+		Port:   a.cfg.DB.Port,
+		User:   a.cfg.DB.User,
+		Pass:   a.cfg.DB.Pass,
+		Name:   a.cfg.DB.Name,
+		Schema: a.cfg.DB.Schema,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	a.db = db
+	return nil
+}
+
+func NewApp(ctx context.Context, cfg config.Config, bot *tgbotapi.BotAPI) (App, error) {
+	a := &app{cfg: cfg, bot: bot, ctx: ctx}
+
+	if err := a.setBot(); err != nil {
+		return nil, err
+	}
 
 	return a, nil
 }
 
-func MustNewApp(cfg config.Config, bot *tgbotapi.BotAPI) App {
-	a, err := NewApp(cfg, bot)
+func MustNewApp(ctx context.Context, cfg config.Config, bot *tgbotapi.BotAPI) App {
+	a, err := NewApp(ctx, cfg, bot)
 	if err != nil {
 		panic(err)
 	}
--- app/contact.go
+++ app/contact.go
@@ -2,10 +2,14 @@ package app
 
 import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"gorm.io/gorm"
 	"taha_tahvieh_tg_bot/config"
+	settingsPort "taha_tahvieh_tg_bot/internal/settings/port"
 )
 
 type App interface {
+	DB() *gorm.DB
 	Config() config.Config
 	Bot() *tgbotapi.BotAPI
+	SettingsService() settingsPort.Service
 }
