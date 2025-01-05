7e73d32b02c1b429a0602594935fd4014012fb88
Hossein Araghi <hoseinaraghi84@gmail.com>
2025-01-05 18:26:12 +0330

    feat(storage): Implement setting repository

---
+++ pkg/adapters/storage/settings_repo.go
@@ -0,0 +1,38 @@
+package storage
+
+import (
+	"context"
+	"github.com/go-gormigrate/gormigrate/v2"
+	"gorm.io/gorm"
+	"taha_tahvieh_tg_bot/internal/settings/domain"
+	"taha_tahvieh_tg_bot/internal/settings/port"
+	"taha_tahvieh_tg_bot/pkg/adapters/storage/mapper"
+	"taha_tahvieh_tg_bot/pkg/adapters/storage/migrations"
+	"taha_tahvieh_tg_bot/pkg/adapters/storage/models"
+)
+
+type settingRepo struct {
+	db *gorm.DB
+}
+
+func NewSettingRepo(db *gorm.DB) port.Repo {
+	return &settingRepo{db}
+}
+
+func (r *settingRepo) FindSettingByTitle(ctx context.Context, title string) (*domain.Setting, error) {
+	var setting models.Setting
+
+	err := r.db.WithContext(ctx).Where("title = ?", title).First(&setting).Error
+
+	return mapper.ToDomainSetting(&setting), err
+}
+
+func (r *settingRepo) UpdateSetting(ctx context.Context, setting domain.Setting) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r *settingRepo) RunMigrations() error {
+	migrator := gormigrate.New(r.db, gormigrate.DefaultOptions, migrations.GetUserMigrations())
+	return migrator.Migrate()
+}
