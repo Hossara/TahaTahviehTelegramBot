97a0e3b77e2ad472c99d92b89cdd541b495fd8fc
Hossein Araghi <hoseinaraghi84@gmail.com>
2025-01-11 20:22:04 +0330

    feat: main keyboard menu

---
+++ server/menus/main.go
@@ -0,0 +1,26 @@
+package menus
+
+var MainMenu = [][]MenuItem{
+	{
+		{Path: "/product_list", IsAdmin: false, Name: "لیست محصولات"},
+	},
+	{
+		{Path: "/register_consultation", IsAdmin: false, Name: "ثبت‌نام برای مشاوره تلفنی"},
+	},
+	{
+		{Path: "/remove_product", IsAdmin: true, Name: "حذف محصول"},
+		{Path: "/add_product", IsAdmin: true, Name: "افزودن محصول"},
+		{Path: "/update_product", IsAdmin: true, Name: "ویرایش محصول"},
+	},
+	{
+		{Path: "/support", IsAdmin: false, Name: "ارتباط‌ با پشتیبانی"},
+		{Path: "/faq", IsAdmin: false, Name: "سوالات متداول"},
+	},
+	{
+		{Path: "/edit_faq", IsAdmin: true, Name: "ویرایش سوالات متداول"},
+	},
+	{
+		{Path: "/about", IsAdmin: false, Name: "درباره ما"},
+		{Path: "/edit_about", IsAdmin: true, Name: "ویرایش درباره ما"},
+	},
+}
