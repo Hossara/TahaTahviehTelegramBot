5763976da1da55bbd85990b4a776195d86a5d6ab
Hossein Araghi <hoseinaraghi84@gmail.com>
2025-01-30 16:33:56 +0330

    feat(keyboard): Implement single column keyboard menu

--- server/keyboards/replys.go
+++ server/keyboards/replys.go
@@ -5,6 +5,20 @@ import (
 	"taha_tahvieh_tg_bot/server/menus"
 )
 
+func InlineKeyboardColumn(menu []menus.MenuItem, isAdmin bool) tgbotapi.InlineKeyboardMarkup {
+	var keyboard []tgbotapi.InlineKeyboardButton
+
+	for _, item := range menu {
+		if item.IsAdmin && !isAdmin {
+			continue
+		}
+
+		keyboard = append(keyboard, tgbotapi.NewInlineKeyboardButtonData(item.Name, item.Path))
+	}
+
+	return tgbotapi.NewInlineKeyboardMarkup(keyboard)
+}
+
 func InlineKeyboard(menu [][]menus.MenuItem, isAdmin bool) tgbotapi.InlineKeyboardMarkup {
 	var keyboardRows [][]tgbotapi.InlineKeyboardButton
 
