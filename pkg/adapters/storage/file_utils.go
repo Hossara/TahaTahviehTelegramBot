157f034fedf36e2c38b3c0d6e596154ad4599cfc
Hossein Araghi <hoseinaraghi84@gmail.com>
2025-02-02 05:05:04 +0330

    refactor(storage): Refactor ExtractFileFormat function and make it usable

--- pkg/adapters/storage/file_utils.go
+++ pkg/adapters/storage/file_utils.go
@@ -1,8 +1,12 @@
 package storage
 
 import (
+	"fmt"
+	"io"
 	"path/filepath"
-	"strings"
+
+	"github.com/h2non/filetype"
+
 	"taha_tahvieh_tg_bot/internal/product_storage/domain"
 )
 
@@ -11,9 +15,23 @@ func ExtractFileName(fullPath string) string {
 	return filepath.Base(fullPath)
 }
 
-// ExtractFileFormat extracts the file format (extension) from a full file path.
-func ExtractFileFormat(fullPath string) string {
-	return strings.TrimPrefix(filepath.Ext(fullPath), ".")
+// ExtractFileFormat extracts the file format (extension)
+func ExtractFileFormat(reader io.Reader) (string, error) {
+	// Read the first 261 bytes (max required for detection)
+	buffer := make([]byte, 261)
+	n, err := reader.Read(buffer)
+	if err != nil && err != io.EOF {
+		return "", fmt.Errorf("error reading file: %v", err)
+	}
+
+	// Detect file type
+	kind, err := filetype.Match(buffer[:n])
+	if err != nil {
+		return "", fmt.Errorf("unknown file type")
+	}
+
+	// Return the extension
+	return kind.Extension, nil
 }
 
 // FileName generates a full file name from a name and extension.
@@ -21,6 +39,6 @@ func FileName(name, extension string) string {
 	return name + "." + extension
 }
 
-func FileToFileName(file domain.File) string {
-	return FileName(file.UUID.String(), file.Format)
+func FileToFilePath(file domain.File) string {
+	return file.Path + FileName(file.UUID.String(), file.Format)
 }
